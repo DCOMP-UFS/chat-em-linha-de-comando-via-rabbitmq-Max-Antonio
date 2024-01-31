75ffaf0adabdbfe54692a51c6db7cbe25ac3d5a9
Max Antônio <114777087+Max-Antonio@users.noreply.github.com>
2024-01-31 00:05:27 -0300

    Update Cliente.java

--- src/main/java/br/ufs/dcomp/ChatRabbitMQ/Cliente.java
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/Cliente.java
@@ -2,13 +2,13 @@ package br.ufs.dcomp.ChatRabbitMQ;
 
 import com.rabbitmq.client.*;
 
-import java.io.IOException;
-
 import java.util.Scanner; 
 
 import java.text.SimpleDateFormat;
 
 import java.util.Date;
+
+import java.io.*;
 
 public class Cliente {
     
@@ -16,13 +16,15 @@ public class Cliente {
     private Channel channel;
     private String username;
     private String receptorAtual; //usuario que o usuario atual está mandando mensagem
+    private String grupoAtual;
     private String QUEUE_NAME;
     private Consumer consumer;
-    
+    private static final String EXCHANGE_NAME = "logs";
     private Scanner sc;
     
     public Cliente(String host, String username) {
         this.username = username;
+        receptorAtual = "";
         try {
             init_comunicacao(host);
         } catch(Exception e) {
@@ -31,13 +33,13 @@ public class Cliente {
     
     private void init_comunicacao(String host) throws Exception{
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost(host); // Alterar
-        factory.setUsername("admin"); // Alterar
-        factory.setPassword("password"); // Alterar
+        factory.setHost(host); 
+        factory.setUsername("admin"); 
+        factory.setPassword("password"); 
         factory.setVirtualHost("/");
 
         connection = factory.newConnection();
-        channel = connection.createChannel();
+        channel = connection.createChannel(); //cria canal
         QUEUE_NAME = "fila@" + username;
         //(queue-name, durable, exclusive, auto-delete, params); 
         channel.queueDeclare(QUEUE_NAME, false,   false,     false,       null);
@@ -62,6 +64,10 @@ public class Cliente {
         receptorAtual = receptor;
     }
     
+    public void setGrupoAtual(String nomeGrupo) {
+        grupoAtual = nomeGrupo;
+    }
+    
     public String getReceptor() {
         return receptorAtual;
     }
@@ -74,14 +80,55 @@ public class Cliente {
         return QUEUE_NAME;
     }
     
-    public void enviarMensagem(String mensagem) throws Exception{
+    public String getGrupoAtual() {
+        return grupoAtual;
+    }
+    
+    public void enviarMensagem(String mensagem) throws Exception{ //montar json na origem
         Date dataAtual = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yy");
         SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
         String dataFormatada = dateFormat.format(dataAtual);
-        String tempoFormatado = timeFormat.format(dataAtual);
-        String mensagemFormatada = "\n(" + dataFormatada + " às " + tempoFormatado + ") " + username + " diz: " + mensagem;
+        String horaFormatada = timeFormat.format(dataAtual);
         
-        channel.basicPublish("",       "fila" + receptorAtual, null,  mensagemFormatada.getBytes("UTF-8"));
+        /*
+        
+        MensagemProto.Conteudo.Builder builderConteudo = MensagemProto.Conteudo.newBuilder();
+        builderConteudo.setTipo(    );
+        builderConteudo.setCorpo(mensagemCorpo);
+        builderConteudo.setNome(    );
+        
+        MensagemProto.Mensagem.Builder builderMensagem = MensagemProto.Mensagem.newBuilder();
+        builderMensagem.setEmissor(username);
+        builderMensagem.setData(dataFormatada);
+        builderMensagem.setHora(horaFormatada);
+        builderMensagem.setGrupo(             );
+        builderMensagem.setConteudo(builderConteudo);
+        
+        */
+        
+        String mensagemFormatada = "\n(" + dataFormatada + " às " + horaFormatada + ") " + username + " diz: " + mensagem;
+        
+        channel.basicPublish("", "fila" + receptorAtual, null,  mensagemFormatada.getBytes("UTF-8"));
+    }
+    
+    public void enviarMensagemGrupo(String mensagem) throws Exception{
+        Date dataAtual = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yy");
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+        String dataFormatada = dateFormat.format(dataAtual);
+        String horaFormatada = timeFormat.format(dataAtual);
+        String mensagemFormatada = "\n(" + dataFormatada + " às " + horaFormatada + ") " + username + "#" + grupoAtual + " diz: " + mensagem;
+        
+        channel.basicPublish(grupoAtual, "", null, mensagemFormatada.getBytes("UTF-8"));
+    }
+    
+    public void addUser(String nomeUser, String nomeGrupo)  throws Exception{
+        channel.queueBind("fila@" + nomeUser, nomeGrupo, "");
+    }
+    
+    public void criaGrupo(String nomeGrupo)  throws Exception{
+        channel.exchangeDeclare(nomeGrupo, "fanout");
+        addUser(username, nomeGrupo); //o criador do grupo é adicionado
     }
 }
